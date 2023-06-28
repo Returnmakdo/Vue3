2b27f2f3331e3960e5c331f2e496450744a9dc90
Chiyoung <cldud970@naver.com>
2023-06-28 13:28:31 +0900

    404Page / 중첩 라우터 설정법

---
+++ src/views/nested/NestedHomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <div class="card-header">Nested Home</div>
+    <div class="card-body">Nested routes Home...</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
