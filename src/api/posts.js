a69ded8e14229b957f769961f4d071f6e7720ae1
Chiyoung <cldud970@naver.com>
2023-07-04 10:48:57 +0900

    put patch 차이

--- src/api/posts.js
+++ src/api/posts.js
@@ -13,7 +13,10 @@ export function createPost(data) {
 }
 
 export function UpdatePost(id, data) {
-  return posts.put(`/${id}`, data);
+  return posts.patch(`/${id}`, data);
+  // put은 전체데이터를 수정하는 것이기 때문에 공백값이 있으면 그대로 읽어버리는 것이기때문에
+  // 등록일이 수정시에 사라지고, patch는 일부분을 수정하는것이기 때문에 값이 넘어오는 것들에 대해서
+  // 수정하는 메서드이다.
 }
 
 export function DeletePost(id) {
