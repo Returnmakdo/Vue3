ae79dc0c2fbacb4296f109ead9cf3be2d9b887bb
Chiyoung <cldud970@naver.com>
2023-07-26 09:34:53 +0900

    pinia

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <h2>About</h2>
+    <h2>About View</h2>
     <p>{{ $route.path }}</p>
-    <p>{{ $route.name }}</p>
-    <button class="btn btn-primary" @click="$router.push('/')">Home으로</button>
+    <button class="btn btn-primary" @click="$router.push('/')">
+      Home으로 이동
+    </button>
+    <h2>Store</h2>
+    <p>counter: {{ counter }}</p>
+    <p>doubleCount: {{ doubleCount }}</p>
+    <p>doubleCountPlusOne: {{ doubleCountPlusOne }}</p>
+    <button @click="increment()">Click!!</button>
   </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
 const route = useRoute();
-console.log('router.path:', route.path);
+console.log('route.path: ', route.path);
+
+const store = useCounterStore();
+
+const { counter, doubleCount, doubleCountPlusOne } = storeToRefs(store);
+const { increment } = store;
+counter.value = 100;
+// increment();
+// increment();
+// increment();
 </script>
 
 <style lang="scss" scoped></style>
